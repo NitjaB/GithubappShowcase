202af0581275236ff6de479c401f4c7877d07f34
Karlo Maricevic <karlomaricevic@gmail.com>
2024-02-03 15:52:36 +0100

    Add code quality checks before push

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
+
+allprojects {
+    apply(from = "$rootDir/codequality/code-quality.gradle")
+}
