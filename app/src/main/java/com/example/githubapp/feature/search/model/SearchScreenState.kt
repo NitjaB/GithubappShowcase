aa61d9c03442c29d76975658d26873611714ef85
Karlo Maricevic <karlomaricevic@gmail.com>
2024-03-01 11:32:13 +0100

    Bring search repo paged data to Search screen

--- app/src/main/java/com/example/githubapp/feature/search/model/SearchScreenState.kt
+++ app/src/main/java/com/example/githubapp/feature/search/model/SearchScreenState.kt
@@ -1,5 +1,11 @@
 package com.example.githubapp.feature.search.model
 
+import androidx.paging.PagingData
+import com.example.githubapp.domain.search.models.Repository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+
 data class SearchScreenState(
     val searchText: String = "",
+    val repositories: Flow<PagingData<Repository>> = flowOf(),
 )
--- app/src/main/java/com/example/githubapp/feature/search/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/example/githubapp/feature/search/viewmodel/SearchViewModel.kt
@@ -1,28 +1,57 @@
 package com.example.githubapp.feature.search.viewmodel
 
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.githubapp.core.base.BaseViewModel
 import com.example.githubapp.core.base.TIMEOUT_DELAY
+import com.example.githubapp.domain.search.usecase.SearchForRepository
 import com.example.githubapp.feature.search.model.SearchScreenEvent
 import com.example.githubapp.feature.search.model.SearchScreenEvent.onOpenFiltersClicked
 import com.example.githubapp.feature.search.model.SearchScreenEvent.onSearchTextChanged
 import com.example.githubapp.feature.search.model.SearchScreenState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
-class SearchViewModel : BaseViewModel<SearchScreenEvent>() {
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val searchForRepository: SearchForRepository,
+) : BaseViewModel<SearchScreenEvent>() {
+
+    private companion object {
+        const val QUERY_DEBOUNCE = 500L
+        const val MINIMAL_QUERY_LENGTH_TO_START_SEARCH = 3
+    }
 
     private val searchText = MutableStateFlow("")
 
-    val state = searchText.map(::SearchScreenState)
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(TIMEOUT_DELAY),
-            initialValue = SearchScreenState()
-        )
+    private val repositories = searchText
+        .debounce(QUERY_DEBOUNCE)
+        .mapLatest { query ->
+            if (query.length >= MINIMAL_QUERY_LENGTH_TO_START_SEARCH) {
+                searchForRepository(query).cachedIn(viewModelScope)
+            } else {
+                flowOf(PagingData.empty())
+            }
+        }
+
+    val state = combine(
+        searchText,
+        repositories,
+        ::SearchScreenState
+    ).stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(TIMEOUT_DELAY),
+        initialValue = SearchScreenState()
+    )
 
     override fun onEvent(event: SearchScreenEvent) {
         when (event) {
