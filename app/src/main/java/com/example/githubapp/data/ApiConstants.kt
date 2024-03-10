d764dec687c2a3433f0eb3fd72423ed7a7b4fe3c
Karlo Maricevic <karlomaricevic@gmail.com>
2024-03-10 15:51:46 +0100

    Add urls and keys needed for autentification
    + extract base api url from network module

---
+++ app/src/main/java/com/example/githubapp/data/ApiConstants.kt
@@ -0,0 +1,13 @@
+package com.example.githubapp.data
+
+object ApiConstants {
+
+    val CLIENT_ID = "da1d75fa20e7e3645858"
+    val CLIENT_SECRET = "024dfb360039c19cb2d8a0e70bb0def56990cb75"
+
+    val BASE_DATA_API_URL = "https://api.github.com/"
+    val BASE_CREDENCAILS_API_URL = "https://github.com/login/oauth/access_token/"
+
+    val BASE_LOGIN_PAGE_URL = "https://github.com/login/oauth/authorize"
+    val BASE_AUTH_REDIRECT_URL = "example://login"
+}
