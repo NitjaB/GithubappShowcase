22aa7753d2e691db60228f2e3d534fde21cb6b9d
Karlo Maricevic <karlomaricevic@gmail.com>
2024-03-22 14:21:38 +0100

    At start of the app send user to login screen if he isn't logged in

--- app/src/main/java/com/example/githubapp/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.githubapp.core.components.GithubappNavigation
+import com.example.githubapp.core.navigation.NavigationEvent
 import com.example.githubapp.core.navigation.Navigator
 import com.example.githubapp.designSystem.theme.GithubAppTheme
+import com.example.githubapp.domain.credencials.usecase.IsUserLoggedIn
+import com.example.githubapp.feature.home.navigation.HomeScreenRouter
+import com.example.githubapp.feature.login.navigation.LoginScreenRouter
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -16,12 +20,20 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var navigator: Navigator
 
+    @Inject
+    lateinit var isUserLoggedIn: IsUserLoggedIn
+
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             GithubAppTheme {
-                GithubappNavigation(navigator)
+                GithubappNavigation(
+                    navigator = navigator,
+                    startScreen = NavigationEvent.Destination(
+                        if (isUserLoggedIn()) HomeScreenRouter.route() else LoginScreenRouter.route()
+                    )
+                )
             }
         }
     }
