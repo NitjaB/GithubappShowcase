c8a6fc9d74fd7c4f49f0c37b5c5548973987f108
Karlo Maricevic <karlomaricevic@gmail.com>
2024-03-24 17:55:03 +0100

    Move page constant to Paged data model and make them public

--- app/src/main/java/com/example/githubapp/data/api/SearchApi.kt
+++ app/src/main/java/com/example/githubapp/data/api/SearchApi.kt
@@ -5,14 +5,11 @@ import com.example.githubapp.data.models.RepositoryResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val DEFAULT_PAGE_SIZE = 10
-private const val FIRST_PAGE = 1
-
 interface SearchApi {
     @GET("search/repositories")
     suspend fun getRepositories(
         @Query("q") query: String,
-        @Query("page") pageNumber: Int = FIRST_PAGE,
-        @Query("per_page") pageSize: Int = DEFAULT_PAGE_SIZE,
+        @Query("page") pageNumber: Int = PagedData.FIRST_PAGE,
+        @Query("per_page") pageSize: Int = PagedData.DEFAULT_PAGE_SIZE,
     ): PagedData<RepositoryResponse>
 }
--- app/src/main/java/com/example/githubapp/data/models/PagedData.kt
+++ app/src/main/java/com/example/githubapp/data/models/PagedData.kt
@@ -6,4 +6,9 @@ data class PagedData<T>(
     @JsonProperty("total_count") val totalCount: Int,
     @JsonProperty("incomplete_results") val incompleteResults: Boolean,
     @JsonProperty("items") val items: List<T>
-)
+) {
+    companion object {
+        const val DEFAULT_PAGE_SIZE = 20
+        const val FIRST_PAGE = 1
+    }
+}
