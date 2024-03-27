d13fbed10b7be73133af2f65df2c16764bb0d645
Karlo Maricevic <karlomaricevic@gmail.com>
2024-03-27 21:09:57 +0100

    Fix themed colors of the app

--- app/src/main/java/com/example/githubapp/designSystem/theme/GithubAppTheme.kt
+++ app/src/main/java/com/example/githubapp/designSystem/theme/GithubAppTheme.kt
@@ -1,15 +1,20 @@
 package com.example.githubapp.designSystem.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = blue,
     surface = gray700,
-    background = black500,
+    background = black,
     error = red,
     onBackground = white,
     onSurface = white,
@@ -18,7 +23,7 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = blue,
     surface = white,
-    background = white,
+    background = gray300,
     error = red,
     onBackground = black,
     onSurface = black,
@@ -32,6 +37,14 @@ fun GithubAppTheme(
     val colorScheme = when {
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.surface.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
     MaterialTheme(
         colorScheme = colorScheme,
