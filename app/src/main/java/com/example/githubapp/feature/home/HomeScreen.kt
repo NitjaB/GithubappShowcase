41404f4c2537cf622988d7bd78ae897e3bf97576
Karlo Maricevic <karlomaricevic@gmail.com>
2024-04-01 13:40:47 +0200

    Preload data for all screen visible in home screen

--- app/src/main/java/com/example/githubapp/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/githubapp/feature/home/HomeScreen.kt
@@ -1,30 +1,34 @@
 package com.example.githubapp.feature.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.githubapp.core.components.GithubappBottomNavigationItem
 import com.example.githubapp.core.components.GithubappNavigationMenu
 import com.example.githubapp.core.navigation.BottomNavigationItem
-import com.example.githubapp.core.components.GithubappBottomNavigationItem
 import com.example.githubapp.feature.overview.OverviewScreen
 import com.example.githubapp.feature.overview.navigation.OverviewScreenRouter
+import com.example.githubapp.feature.overview.viewmodel.OverviewViewModel
 import com.example.githubapp.feature.profile.ProfileScreen
 import com.example.githubapp.feature.profile.navigation.ProfileScreenRouter
+import com.example.githubapp.feature.profile.viewmodel.ProfileViewModel
 import com.example.githubapp.feature.search.SearchScreen
 import com.example.githubapp.feature.search.navigation.SearchScreenRouter
+import com.example.githubapp.feature.search.viewmodel.SearchViewModel
 
 @Composable
 fun HomeScreen() {
     val navController = rememberNavController()
+
+    val overviewViewModel: OverviewViewModel = hiltViewModel()
+    val searchViewModel: SearchViewModel = hiltViewModel()
+    val profileViewModel: ProfileViewModel = hiltViewModel()
+
     Scaffold(
         bottomBar = {
             GithubappNavigationMenu {
@@ -44,9 +48,24 @@ fun HomeScreen() {
                 navController = navController,
                 startDestination = OverviewScreenRouter.route(),
             ) {
-                composable(OverviewScreenRouter.route()) { OverviewScreen(paddingValues = innerPadding) }
-                composable(SearchScreenRouter.route()) { SearchScreen(innerPadding) }
-                composable(ProfileScreenRouter.route()) { ProfileScreen(innerPadding) }
+                composable(OverviewScreenRouter.route()) {
+                    OverviewScreen(
+                        viewModel = overviewViewModel,
+                        paddingValues = innerPadding
+                    )
+                }
+                composable(SearchScreenRouter.route()) {
+                    SearchScreen(
+                        innerPadding = innerPadding,
+                        viewModel = searchViewModel,
+                    )
+                }
+                composable(ProfileScreenRouter.route()) {
+                    ProfileScreen(
+                        paddingValues = innerPadding,
+                        viewModel = profileViewModel,
+                    )
+                }
             }
         },
     )
