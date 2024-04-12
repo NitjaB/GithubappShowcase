ef2ec131d1893f1b0ad1188bff820df1b6cf8200
Nitja Berakovic <nberakovic@gmail.com>
2024-04-12 19:36:21 +0200

    Create repository details screen

--- app/src/main/java/com/example/githubapp/feature/repositoryDetails/DetailsScreen.kt
+++ app/src/main/java/com/example/githubapp/feature/repositoryDetails/DetailsScreen.kt
@@ -1,10 +1,73 @@
 package com.example.githubapp.feature.repositoryDetails
 
 import androidx.compose.runtime.Composable
-
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.githubapp.R
+import com.example.githubapp.feature.repositoryDetails.components.RepositoryDetailsHeadline
+import com.example.githubapp.feature.repositoryDetails.models.DetailsScreenEvent
+import com.example.githubapp.feature.repositoryDetails.models.RepositoryDetailsVMParam
+import com.example.githubapp.feature.repositoryDetails.viewmodel.DetailsViewModel
 @Composable
 fun DetailsScreen(
     owner: String,
     repoName: String,
 ) {
+    val viewModel = hiltViewModel<DetailsViewModel, DetailsViewModel.RepositoryDetailsViewModelFactory> { factory ->
+        factory.create(
+            RepositoryDetailsVMParam(owner, repoName)
+        )
+    }
+    val viewState by viewModel.viewState.collectAsState()
+    Column(
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_arrow_left),
+                contentDescription = stringResource(R.string.default_icon_content_description),
+                modifier = Modifier
+                    .padding(4.dp)
+                    .clip(CircleShape)
+                    .clickable { viewModel.onEvent(DetailsScreenEvent.NavigateBack) }
+                    .padding(4.dp)
+                    .size(30.dp)
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.ic_settings),
+                contentDescription = stringResource(id = R.string.default_icon_content_description),
+                modifier = Modifier
+                    .padding(6.dp)
+                    .clip(CircleShape)
+                    .clickable { viewModel.onEvent(DetailsScreenEvent.OnShareClicked) }
+                    .padding(4.dp)
+                    .size(25.dp)
+            )
+        }
+        viewState.details?.let { notNullRepository ->
+            RepositoryDetailsHeadline(
+                notNullRepository,
+                viewState.isStarredByUser,
+            )
+        }
+    }
 }
---
+++ app/src/main/java/com/example/githubapp/feature/repositoryDetails/components/RepositoryDetailsHeadline.kt
@@ -0,0 +1,231 @@
+package com.example.githubapp.feature.repositoryDetails.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.githubapp.R
+import com.example.githubapp.core.components.Image
+import com.example.githubapp.designSystem.theme.gray500
+import com.example.githubapp.domain.models.AuthorInfo
+import com.example.githubapp.domain.search.models.Repository
+
+@Composable
+fun RepositoryDetailsHeadline(
+    repository: Repository,
+    isStarred: Boolean = false,
+    isStaredLoading: Boolean = false,
+) {
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                image = repository.author.avatar,
+                contentDescription = stringResource(R.string.default_image_content_description),
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(24.dp)
+            )
+            Text(
+                text = repository.author.username,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                color = gray500,
+                modifier = Modifier.padding(
+                    start = 4.dp,
+                    end = 80.dp,
+                )
+            )
+        }
+        Row {
+            Text(
+                text = repository.name,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                modifier = Modifier.padding(end = 24.dp, top = 8.dp),
+                style = MaterialTheme.typography.headlineLarge
+            )
+        }
+        Row {
+            if (!repository.description.isNullOrEmpty()) {
+                Text(
+                    text = repository.description,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 3,
+                    modifier = Modifier.padding(top = 4.dp),
+                    style = MaterialTheme.typography.bodyLarge.copy(color = gray500)
+                )
+            }
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Icon(
+                painter = painterResource(
+                    id = R.drawable.ic_url
+                ),
+                contentDescription = stringResource(id = R.string.default_image_content_description),
+                tint = gray500
+            )
+            Text(
+                text = repository.url ?: "",
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                modifier = Modifier.padding(end = 24.dp, start = 4.dp),
+                style = MaterialTheme.typography.titleMedium
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(top = 16.dp)
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_star),
+                contentDescription = stringResource(R.string.default_icon_content_description),
+                tint = gray500,
+            )
+            Text(
+                text = repository.staredTimes.toString(),
+                Modifier.padding(horizontal = 4.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.repository_details_screen_star),
+                color = gray500
+            )
+            Icon(
+                painter = painterResource(id = R.drawable.ic_fork),
+                contentDescription = stringResource(R.string.default_icon_content_description),
+                tint = gray500,
+                modifier = Modifier.padding(end = 4.dp, start = 8.dp)
+            )
+            Text(text = repository.forkNumber.toString())
+            Text(
+                text = stringResource(id = R.string.repository_details_screen_fork),
+                Modifier.padding(horizontal = 4.dp),
+                color = gray500
+            )
+        }
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp),
+            onClick = { /*TODO*/ },
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.surface),
+            shape = RoundedCornerShape(4.dp)
+        ) {
+            if (!isStaredLoading) {
+                Icon(
+                    painter = painterResource(
+                        if (isStarred) {
+                            R.drawable.ic_star_filled
+                        } else {
+                            R.drawable.ic_star
+                        }
+                    ),
+                    tint = gray500,
+                    modifier = Modifier.padding(end = 6.dp),
+                    contentDescription = stringResource(id = R.string.default_icon_content_description),
+                )
+            } else {
+                CircularProgressIndicator(
+                    Modifier
+                        .padding(end = 6.dp)
+                        .size(16.dp)
+                )
+            }
+            Text(
+                text = stringResource(id = R.string.repository_details_screen_star_button),
+                color = gray500
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun RepositoryDetailsHeadlinePreview() {
+    RepositoryDetailsHeadline(
+        Repository(
+            id = 1,
+            name = "RepoName",
+            author = AuthorInfo(
+                username = "Karlo",
+                avatar = com.example.githubapp.domain.models.Image.LocalImage(R.drawable.ic_launcher_foreground),
+            ),
+            fullName = "Karlo/Kotlin",
+            description = "Description",
+            language = "",
+            staredTimes = 1900,
+            forkNumber = 150,
+            url = "www.github.com"
+        )
+    )
+}
+
+@Preview
+@Composable
+private fun RepositoryDetailsHeadlineLongPreview() {
+    RepositoryDetailsHeadline(
+        Repository(
+            id = 1,
+            name = List(10) { "LongRepoName" }.joinToString(""),
+            author = AuthorInfo(
+                username = List(10) { "LongUsername" }.joinToString(""),
+                avatar = com.example.githubapp.domain.models.Image.LocalImage(R.drawable.ic_launcher_foreground),
+            ),
+            fullName = "Karlo/Kotlin",
+            description = List(30) { "LongDescription" }.joinToString(""),
+            language = "Kotlin",
+            staredTimes = 1900,
+            forkNumber = 150,
+            url = "cghdsjgcdhyugdyhukagDHSKUGYFDUSGYDGSYAKHJDIGHUfgfdghgfhdghfgdhfgdhj"
+
+        )
+    )
+}
+
+@Preview
+@Composable
+private fun RepositoryDetailsLoadingStarredHeadlineLongPreview() {
+    RepositoryDetailsHeadline(
+        Repository(
+            id = 1,
+            name = List(10) { "LongRepoName" }.joinToString(""),
+            author = AuthorInfo(
+                username = List(10) { "LongUsername" }.joinToString(""),
+                avatar = com.example.githubapp.domain.models.Image.LocalImage(R.drawable.ic_launcher_foreground),
+            ),
+            fullName = "Karlo/Kotlin",
+            description = List(30) { "LongDescription" }.joinToString(""),
+            language = "Kotlin",
+            staredTimes = 1900,
+            forkNumber = 150,
+            url = "cghdsjgcdhyugdyhukagDHSKUGYFDUSGYDGSYAKHJDIGHUfgfdghgfhdghfgdhfgdhj"
+        ),
+        isStaredLoading = true,
+    )
+}
