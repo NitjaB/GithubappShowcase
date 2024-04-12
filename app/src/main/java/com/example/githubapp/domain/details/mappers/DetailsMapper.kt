58081aebf6ac3af9a5d8f0266872841fbf4cf379
Nitja Berakovic <nberakovic@gmail.com>
2024-04-12 19:16:42 +0200

    Create repository mapper

---
+++ app/src/main/java/com/example/githubapp/domain/details/mappers/DetailsMapper.kt
@@ -0,0 +1,25 @@
+package com.example.githubapp.domain.details.mappers
+
+import com.example.githubapp.data.models.RepositoryResponse
+import com.example.githubapp.domain.models.AuthorInfo
+import com.example.githubapp.domain.models.Image
+import com.example.githubapp.domain.search.models.Repository
+import javax.inject.Inject
+
+class DetailsMapper @Inject constructor(){
+    fun map(response: RepositoryResponse) = Repository(
+        id = response.id,
+        name = response.name,
+        url = response.url,
+        author = AuthorInfo(
+            response.owner.username,
+            Image.RemoteImage(response.owner.avatarUrl)
+        ),
+        fullName = response.fullName,
+        description = response.description,
+        language = response.language,
+        staredTimes = response.stargazersCount,
+        forkNumber = response.forksCount
+
+    )
+}
